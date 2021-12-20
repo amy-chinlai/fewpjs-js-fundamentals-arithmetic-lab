99b49257c544ac07239355f2b2292b34766517f5
amy-chinlai <amy@actionnetwork.org>
2021-12-20 11:45:11 -0500

    commit tree b9a5bf53169272dc3edc0669d0e2c529bb8121e8

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// challenge 1
+let num1 = 2
+let num2 = 31
+
+var multiply = num1 * num2
+
+// challenge 2
+var random = Math.ceil(Math.random())
+
+// challenge 3
+let num3 = 16
+let num4 = 12
+
+var mod = num3 % num4
+
+// challenge 4
+var max = Math.max(1, 2, 20)
